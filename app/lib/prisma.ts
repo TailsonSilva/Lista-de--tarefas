cfb7330cfb524a8754a3cf7ce9bf1020e01b9c49
TailsonSIlva <tailsoncs232@gmail.com>
2026-01-05 10:59:51 -0300

    Fix: implementando adaptador pg para Prisma 7

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,12 +1,18 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined
 }
 
+const connectionString = process.env.DATABASE_URL
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
 export const db =
   globalForPrisma.prisma ||
-  new PrismaClient()
+  new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
 
